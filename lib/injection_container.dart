c06cdd7bdd4835531b9234c6ce953a300f4a18e3
flutterry <ahmed.essam.shafi@gmail.com>
2022-11-22 17:49:54 +0200

    injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,14 @@
+import 'package:aq_admin/features/auth/data/datasource/remote_auth.datasource.dart';
+import 'package:aq_admin/features/auth/domain/auth.repository.dart';
+import 'package:aq_admin/features/estate/domain/estate.repository.dart';
 import 'package:get_it/get_it.dart';
+
 final sl = GetIt.I;
-void initiate() {}
+
+void initiate() {
+  sl
+    // ..registerLazySingleton<AuthRepository>(() => AuthRepositoryImp())
+    ..registerLazySingleton<RemoteAuthDataSource>(() => FakeAuthDataSource());
+}
+
+// sl<AuthRepository>().login();
